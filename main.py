a55a7466e2f1d9ecba517761e49c120d90c1a1ef
Tee55 <teerapaths@gmail.com>
2023-11-09 23:15:58 +0700

    main

--- main.py
+++ main.py
@@ -7,38 +7,8 @@ from GAN import GAN
 import imageio
 import keras_core as keras
 import tensorflow as tf
-import torch
-from torchvision.utils import save_image
-from torchvision import transforms
-import numpy as np
-import cv2
 
-dataset = keras.utils.image_dataset_from_directory(
-    "face_images", label_mode=None, image_size=(64, 64), batch_size=32
-)
-
-def plot_dataset(num_samples):
-    plt.figure(figsize=(10, 10))
-    for images, labels in dataset.take(1):
-        for i in range(num_samples):
-            plt.subplot(4, 4, i+1)
-            plt.imshow(images[i].numpy().astype("uint8"))
-            plt.axis("off")
-    plt.show()
-
-#plot_dataset(16)
-'''
-def process(image, label):
-    image = tf.cast(image/255. ,tf.float32)
-    return image, label
-
-dataset = dataset.map(process)
-'''
-
-# normalize the images
-dataset = dataset.map(lambda x: (x - 127.5) / 127.5)
-
-latent_dim = 100
+latent_dim = 128
 def create_generator():
     model = keras.Sequential(name="Generator")
     model.add(keras.layers.Dense(8 * 8 * 512, input_shape=(latent_dim, )))
@@ -78,39 +48,48 @@ def create_discriminator():
     print(model.summary())
     return model
 
+dataset = keras.utils.image_dataset_from_directory(
+    "face_images", image_size=(64, 64), batch_size=32
+)
+
+def plot_dataset(num_samples):
+    plt.figure(figsize=(10, 10))
+    for images, labels in dataset.take(1):
+        for i in range(num_samples):
+            plt.subplot(4, 4, i+1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.axis("off")
+    plt.show()
+
+#plot_dataset(16)
+def process(image, label):
+    image = tf.cast(image/255.0 , tf.float32)
+    return image, label
+
+dataset = dataset.map(process)
+
 generator = create_generator()
 discriminator = create_discriminator()
-toPILImage = transforms.ToPILImage()
 noise = keras.random.normal(shape=(1, latent_dim), mean=0.0, stddev=0.02)
 
-def plot_generator():
-    generated_images = generator(noise, training=False)
-    generated_images *= (generated_images * 127.5) + 127.5
-    output = generated_images[0].cpu().detach().numpy()
-    img = keras.utils.array_to_img(output)
-    img.save("./generated_images/generated_image_{}.png".format(0))
+writer = imageio.get_writer(
+    './generated_images/{}.gif'.format(time.strftime("%Y%M%d-%H%M%S")), mode='I')
 
-plot_generator()
+class GANMonitor(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        generated_images = self.model.generator(noise)
+        generated_images *= 255.0
+        output = generated_images[0].detach().cpu().numpy()
+        img = keras.utils.array_to_img(output)
+        img.save("./generated_images/generated_image_{}.png".format(epoch))
+        writer.append_data(img)
 
 gan = GAN(discriminator=discriminator,
           generator=generator, latent_dim=latent_dim)
 gan.compile(
     d_optimizer=keras.optimizers.Adam(learning_rate=0.0001),
-    g_optimizer=keras.optimizers.Adam(learning_rate=0.0003),
-    loss_fn=keras.losses.BinaryCrossentropy(),
+    g_optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+    loss_fn=keras.losses.BinaryCrossentropy(from_logits=True),
 )
-
-writer = imageio.get_writer(
-    './generated_images/{}.gif'.format(time.strftime("%Y%M%d-%H%M%S")), mode='I')
-
-
-class GANMonitor(keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs=None):
-        generated_images = self.model.generator(noise)
-        generated_images *= (generated_images * 127.5) + 127.5
-        output = generated_images[0].cpu().detach().numpy()
-        img = keras.utils.array_to_img(output)
-        img.save("./generated_images/generated_image_{}.png".format(epoch))
-
-gan.fit(dataset, epochs=20, callbacks=[GANMonitor()])
+gan.fit(dataset.take(500), epochs=20, callbacks=[GANMonitor()])
 writer.close()
